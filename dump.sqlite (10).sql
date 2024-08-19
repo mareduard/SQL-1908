704dc7ec52a4548d34af07d61f20e92e898009b8
mareduard <159054835+mareduard@users.noreply.github.com>
2024-08-19 15:13:54 -0300

    Add files via upload

---
+++ dump.sqlite (10).sql
@@ -0,0 +1,14 @@
+-- TABLE
+CREATE TABLE curiosidades_paris2024(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  curiosidade TEXT NOT NULL,
+  categoria TEXT
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
